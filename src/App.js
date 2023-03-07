2651d5d595257f73fd3e4204a581035b86dc6141
circlebytes <circlebytes.iftala@gmail.com>
2023-03-08 00:37:38 +0700

    feat(pre-event): learn JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{
+      margin: "auto",
+      width: 800,
+      paddingTop: "1rem",
+    }}>
+      <h1 className='title'>Nyoba Ajach</h1>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Iftala</th>
+            <th>Teknik Informatika</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>Iftala</th>
+            <th>Teknik Informatika</th> 
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
