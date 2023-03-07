e3c1a225911f37f4717d16ebdd7c44aa7467b27b
circlebytes <circlebytes.iftala@gmail.com>
2023-03-08 00:23:25 +0700

    Updates

---
+++ precache-manifest.2f8d0fa62f854c10e44b13febdcfb268.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6723c06e30b8cb8f279",
+    "url": "/react/static/css/main.f777253c.chunk.css"
+  },
+  {
+    "revision": "f6723c06e30b8cb8f279",
+    "url": "/react/static/js/main.f6723c06.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/react/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/react/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "26240bb31a828807c2c8",
+    "url": "/react/static/js/runtime~main.26240bb3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d580f7fb55d96a878f8f6640b4032d3a",
+    "url": "/react/index.html"
+  }
+];
