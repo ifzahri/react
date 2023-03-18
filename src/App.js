634036a8cb5c060585f27f60a4f3fd4ba3e61f5a
circlebytes <circlebytes.iftala@gmail.com>
2023-03-19 00:00:24 +0700

    feat(pre-event): remodel PropTypes with useContext and Reducer

--- src/App.js
+++ src/App.js
@@ -1,68 +1,35 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "@emotion/styled";
-import { Button, CssBaseline } from "@material-ui/core";
+import { CssBaseline } from "@material-ui/core";
 
 import "./App.css";
 
-const PokemonType = PropTypes.shape({
-  id: PropTypes.string.isRequired,
-  name: PropTypes.shape({
-    english: PropTypes.string.isRequired,
-    japanese: PropTypes.string.isRequired,
-    chinese: PropTypes.string.isRequired,
-    french: PropTypes.string.isRequired,
-  }),
-  type: PropTypes.arrayOf(PropTypes.string.isRequired),
-  base: PropTypes.shape({
-    HP: PropTypes.number.isRequired,
-    Attack: PropTypes.number.isRequired,
-    Defense: PropTypes.number.isRequired,
-    "Sp. Attack": PropTypes.number.isRequired,
-    "Sp. Defense": PropTypes.number.isRequired,
-    Speed: PropTypes.number.isRequired,
-  }),
-});
+import PokemonInfo from "./components/PokemonInfo";
+import PokemonFilter from "./components/PokemonFilter";
+import PokemonTable from "./components/PokemonTable";
+import PokemonContext from "./PokemonContext";
 
-const PokemonRow = ({ pokemon, onClick }) => (
-  <>
-    <tr key={pokemon.id}>
-      <td>{pokemon.name.english}</td>
-      <td>{pokemon.type.join(", ")}</td>
-      <td>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => onClick(pokemon)}
-        >
-          More Information
-        </Button>
-      </td>
-    </tr>
-  </>
-);
-
-PokemonRow.propTypes = {
-  pokemon: PropTypes.arrayOf(PokemonType),
+const pokemonReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_FILTER":
+      return {
+        ...state,
+        filter: action.payload,
+      };
+    case "SET_POKEMON":
+      return {
+        ...state,
+        pokemon: action.payload,
+      };
+    case "SET_SELECTED_POKEMON":
+      return {
+        ...state,
+        selectedPokemon: action.payload,
+      };
+    default:
+      throw new Error("No Action");
+  }
 };
-
-const PokemonInfo = ({ name: { english }, base }) => (
-  <div>
-    <h2>{english}</h2>
-    <table>
-      <tbody>
-        {Object.keys(base).map((key) => (
-          <tr key={key}>
-            <td>{key}</td>
-            <td>{base[key]}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-);
-
-PokemonInfo.propTypes = PokemonType;
 
 const Title = styled.h1`
   text-align: center;
@@ -77,59 +44,48 @@ const TwoColumnLayout = styled.div`
   grid-template-columns: 80% 20%;
   grid-column-gap: 1rem;
 `;
-const Input = styled.input`
-  width: 100%;
-  padding: 0.2rem;
-  font-size: large;
-`;
 
 function App() {
-  const [filter, filterSet] = React.useState("");
-  const [pokemon, pokemonSet] = React.useState(null);
-  const [selectedPokemon, selectedPokemonSet] = React.useState(null);
+  const [state, dispatch] = React.useReducer(pokemonReducer, {
+    pokemon: [],
+    filter: "",
+    selectedPokemon: null,
+  });
 
   React.useEffect(() => {
     fetch("/starting-react/pokemon.json")
       .then((resp) => resp.json())
-      .then((data) => pokemonSet(data));
+      .then((data) =>
+        dispatch({
+          type: "SET_POKEMON",
+          payload: data,
+        })
+      );
   }, []);
 
-  if (!pokemon) {
+  if (!state.pokemon) {
     return <div>Loading data</div>;
   }
 
   return (
-    <PageContainer>
-      <CssBaseline />
-      <Title>Pokemon Search</Title>
-      <TwoColumnLayout>
-        <div>
-          <Input
-            type="text"
-            value={filter}
-            onChange={(evt) => filterSet(evt.target.value)}
-          />
-          <table width="100%">
-            <tbody>
-              {pokemon
-                .filter(({ name: { english } }) =>
-                  english
-                    .toLocaleLowerCase()
-                    .includes(filter.toLocaleLowerCase())
-                )
-                .slice(0, 20)
-                .map((pokemon) => (
-                  <PokemonRow
-                    pokemon={pokemon}
-                    onClick={(pokemon) => selectedPokemonSet(pokemon)}
-                  />
-                ))}
-            </tbody>
-          </table>
-        </div>
-        {selectedPokemon && <PokemonInfo {...selectedPokemon} />}
-      </TwoColumnLayout>
-    </PageContainer>
+    <PokemonContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      <PageContainer>
+        <CssBaseline />
+        <Title>Pokemon Search</Title>
+        <TwoColumnLayout>
+          <div>
+            <PokemonFilter />
+            <PokemonTable />
+          </div>
+          <PokemonInfo />
+        </TwoColumnLayout>
+      </PageContainer>
+    </PokemonContext.Provider>
   );
 }
 
